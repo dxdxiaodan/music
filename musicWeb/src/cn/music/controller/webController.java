d52f3cacee1d3f0e2e5dfbe6c96fdc0dd5d81dc4
dxd <1650122914@qq.com>
2017-06-16 17:34:52 +0800

    基础配置更新-1

---
+++ musicWeb/src/cn/music/controller/webController.java
@@ -0,0 +1,8 @@
+package cn.music.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class webController {
+
+}
---
+++ musicWeb/src/cn/music/test/common/SpringJunitTest.java
@@ -0,0 +1,16 @@
+package cn.music.test.common;
+
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 
+ * @author 老戴
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Configuration(locations="")
+public class SpringJunitTest {
+
+}
