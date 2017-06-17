e366671e83767d57701a290a6c8eff1e8b364192
dxd <1650122914@qq.com>
2017-06-17 10:22:09 +0800

    修改错误

--- musicWeb/src/common/SpringJunitTest.java
+++ musicWeb/src/common/SpringJunitTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:musicWeb/src/common/SpringJunitTest.java
 package common;
 
 import org.junit.runner.RunWith;
@@ -16,22 +15,4 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class SpringJunitTest {
 
 }
-=======
-package common;
 
-import org.junit.runner.RunWith;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-/**
- * 
- * @author 老戴
- *
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:spring/spring.dao.xml")
-public class SpringJunitTest {
-
-}
->>>>>>> 6c83d5f8f894df364629558095a119beccec6ad1:musicWeb/src/common/SpringJunitTest.java
