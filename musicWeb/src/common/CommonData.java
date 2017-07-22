28bb077ddf8b9be7bef638a4da8499268d836bec
dxd <1650122914@qq.com>
2017-07-22 20:18:36 +0800

    更新公用数据文件

---
+++ musicWeb/src/common/CommonData.java
@@ -0,0 +1,15 @@
+package common;
+/*
+ * @dxd
+ * 用于固定值的定义，例如曲风，语种种类定义
+ */
+public class CommonData {
+	public static final String[] style={"古风","摇滚","民谣","古典","轻音乐"}; 
+	public static final String[] language={"华语","粤语","英语","日语","德语"}; 
+	public static final String[] downloadPermissions={"下载"}; 
+	public static final String[] category ={"华语","粤语","英语","日语","德语"}; 
+	public static final String[] amountStatus={"使用","停用","禁用"}; 
+	public static final String[] checkStatus={"审核通过","审核失败"}; 
+
+	
+ }
