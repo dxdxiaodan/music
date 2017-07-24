6bcfa2d5272163ad3acb43e372811945eed57c63
lhj <lhj@name-PC>
2017-07-24 10:17:45 +0800

    常量改为大写

--- musicWeb/src/common/CommonData.java
+++ musicWeb/src/common/CommonData.java
@@ -4,12 +4,12 @@ package common;
  * 用于固定值的定义，例如曲风，语种种类定义
  */
 public class CommonData {
-	public static final String[] style={"古风","摇滚","民谣","古典","轻音乐","其他"}; 
-	public static final String[] language={"华语","粤语","英语","日语","德语","其他"}; 
-	public static final String[] downloadPermissions={"下载"}; 
-	public static final String[] category ={"原创","翻唱","伴奏","其他"}; 
-	public static final String[] amountStatus={"使用","停用","禁用"}; 
-	public static final String[] checkStatus={"审核通过","审核失败"}; 
-
+	public static final String[] STYLE={"古风","摇滚","民谣","古典","轻音乐","其他"}; 
+	public static final String[] LANGUAGE={"华语","粤语","英语","日语","德语","其他"}; 
+	public static final String[] DOWNLOADPERMISSIONS={"免费","付费"}; 
+	public static final String[] CATEGORY ={"原创","翻唱","伴奏","其他"}; 
+	public static final String[] AMOUNTSTATUS={"使用","停用","禁用"}; 
+	public static final String[] CHECKSTATUS={"审核通过","审核失败"}; 
+	public static final String[] COMMENTSTATUS={"审核中","成功","失败"};
 	
  }
